076cb657222787c1d4108b6081c2a3b06680b94c
Johnny <jthompson@tmg.dev>
2021-05-19 10:42:51 -0700

    Project files after filming Random tutorial

--- EcsTutorials-Project/Assets/Random/Scripts/Systems/CentralRandomSystem.cs
+++ EcsTutorials-Project/Assets/Random/Scripts/Systems/CentralRandomSystem.cs
@@ -6,27 +6,30 @@ namespace TMG.ECS_Random
 {
     public class CentralRandomSystem : SystemBase
     {
+        private CentralRandomData _centralRandom;
         
         protected override void OnStartRunning()
         {
-            var centralRandom = GetSingleton<CentralRandomData>();
-            centralRandom.Value.InitState(10);
-            Debug.Log($"Initial State: {centralRandom.Value.state}");
+            _centralRandom = GetSingleton<CentralRandomData>();
+            _centralRandom.Value.InitState(10);
+            Debug.Log($"Initial State: {_centralRandom.Value.state}");
         }
 
         protected override void OnUpdate()
         {
             if (Input.GetKeyDown(KeyCode.R))
             {
-                var centralRandom = GetSingleton<CentralRandomData>();
-                Debug.Log($"Captured State: {centralRandom.Value.state}");
+                var capturedRandom = _centralRandom;
+                Debug.Log($"Captured State: {capturedRandom.Value.state}");
                 Entities
                     .WithAll<CentralRandomTag>()
                     .ForEach((ref Translation translation) =>
                     {
-                        translation.Value.x = centralRandom.Value.NextFloat(0, 25f);
+                        translation.Value.x = capturedRandom.Value.NextFloat(0, 25f);
                     }).ScheduleParallel();
-                SetSingleton(centralRandom);
+                
+                Dependency.Complete();
+                SetSingleton(capturedRandom);
             }
         }
     }
