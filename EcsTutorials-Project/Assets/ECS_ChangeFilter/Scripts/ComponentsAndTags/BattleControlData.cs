d53eabaf654278eb3c994d92f989acbd6b0b0710
Johnny <jthompson@tmg.dev>
2021-07-04 10:17:55 -0700

    Implemented JRPG Battle Scene to demonstrate use of change filters.

---
+++ EcsTutorials-Project/Assets/ECS_ChangeFilter/Scripts/ComponentsAndTags/BattleControlData.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+
+namespace TMG.ChangeFilter
+{
+    [GenerateAuthoringComponent]
+    public struct BattleControlData : IComponentData
+    {
+        public Entity Enemy1;
+        public Entity Enemy2;
+        public Entity Enemy3;
+        public Entity Enemy4;
+        public int AttackPower;
+    }
+}
---
+++ EcsTutorials-Project/Assets/ECS_ChangeFilter/Scripts/Systems/ChangeFilterSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace TMG.ChangeFilter
+{
+    public class ChangeFilterSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities
+                .WithChangeFilter<EnemyHealthData, Translation>()
+                .ForEach((Entity e, EnemyHealthUI ui, ref EnemyHealthData healthData) =>
+            {
+                Debug.Log($"Updating Health UI on: {e.Index}");
+                ui.Slider.value = healthData.Value;
+            }).WithoutBurst().Run();
+        }
+    }
+}
