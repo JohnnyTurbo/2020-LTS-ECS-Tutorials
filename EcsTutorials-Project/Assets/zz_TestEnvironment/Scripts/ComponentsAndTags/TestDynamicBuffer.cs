c59e9bfddbf8974419c2d508955a241a039220b9
Johnny <jthompson@tmg.dev>
2021-07-21 07:33:07 -0700

    Testing some things.

---
+++ EcsTutorials-Project/Assets/zz_TestEnvironment/Scripts/ComponentsAndTags/TestDynamicBuffer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Unity.Entities;
+
+namespace TMG.zz_Test_Env
+{
+    [GenerateAuthoringComponent]
+    public struct TestDynamicBuffer : IBufferElementData
+    {
+        public Workaround valueSet;
+    }
+
+    [Serializable]
+    public struct Workaround
+    {
+        public int valueInt;
+        public bool valueBool;
+    }
+}
