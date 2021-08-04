77755fc5a408954b0a3c815dbd2d443a95045daf
Johnny <jthompson@tmg.dev>
2021-08-03 19:53:06 -0700

    Experimentation with ECS transforms.

---
+++ EcsTutorials-Project/Assets/ECS_Transforms/Scripts/ComponentsAndTags/TestMoveData.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace TMG.ECS_Transforms
+{
+    [GenerateAuthoringComponent]
+    public struct TestMoveData : IComponentData
+    {
+        public Entity EntityToMove;
+        public float MoveSpeed;
+        public KeyCode MoveKey;
+
+        public float RoationSpeed;
+        public KeyCode RotationKey;
+    }
+}
