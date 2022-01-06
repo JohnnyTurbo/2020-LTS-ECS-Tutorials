2bf3630c2ab573265d303acf3a7ddc3e5725f637
Johnny <jthompson@tmg.dev>
2022-01-06 09:21:21 -0800

    Copied JobDependency files over to URP project for use with HybridRenderer V2.

---
+++ ECSTutorials-URP-Project/Assets/ECS_JobDependencies/Scripts/ComponentsAndTags/PlayerInputData.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+namespace TMG.JobDependencies
+{
+    [GenerateAuthoringComponent]
+    public struct PlayerInputData : IComponentData
+    {
+        public float MovementThisFrame;
+        public float RotationThisFrame;
+    }
+}
