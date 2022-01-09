069eb28a7c57944c0e5f57d2cc13b2dc8ae72132
Johnny <jthompson@tmg.dev>
2022-01-08 23:07:39 -0800

    Created a simple Job.WithCode() demonstration.

---
+++ EcsTutorials-Project/Assets/ECS_JobWithCode/Scripts/ComponentsAndTags/SpherePrefabData.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+namespace ECS_JobWithCode.Scripts.ComponentsAndTags
+{
+    [GenerateAuthoringComponent]
+    public struct SpherePrefabData : IComponentData
+    {
+        public Entity Prefab;
+        public int SpawnCount;
+    }
+}
